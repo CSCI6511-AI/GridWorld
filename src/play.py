abc3b604e53d271472bb5da922a7821e714529a6
CharlotteSun0530 <charlottesun0530@gmail.com>
2023-04-22 21:05:34 -0400

    master_play

--- src/play.py
+++ src/play.py
@@ -23,13 +23,33 @@ def play(environment, agent, max_steps_per_episode=1000, learn=False):
         cumulative_reward += reward
         step += 1
 
-        if environment.check_state() == 'TERMINAL':  # If game is in terminal state, game over and start next trial
-            environment.__init__()
-            game_over = True
+        # 获取当前位置和得分
+        loc = get_location()
+        print("当前位置：", loc["location"], "得分：", get_score())
 
-        elif environment.check_state() == 'EXIT':  # 如果遇到了出口，则结束当前游戏并开始下一个试验
-            environment.__init__()
-            game_over = True
+        # 执行测试
+        while True:
+            # 进行一些操作
+            # ...
+
+            # 获取执行动作后的结果
+            res = make_a_move(move, loc["world"])
+            print("移动方向：", move, "得分增量：", res["scoreIncrement"], "新位置：", res["newState"])
+            loc = get_location()
+            print("当前位置：", loc["location"], "得分：", get_score())
+
+            # 判断是否已到达终点
+            if loc["location"] == "END":
+                print("已到达终点！得分：", get_score())
+                break
+
+        # if environment.check_state() == 'TERMINAL':  # If game is in terminal state, game over and start next trial
+        #     environment.__init__()
+        #     game_over = True
+        #
+        # elif environment.check_state() == 'EXIT':  # 如果遇到了出口，则结束当前游戏并开始下一个试验
+        #     environment.__init__()
+        #     game_over = True
 
     reward_per_episode.append(cumulative_reward)  # Append reward for current trial to performance log
 
