49c13796efc57b8e31330d8f8a7bd0767cd590c2
Maolinxin001 <112763694+Maolinxin001@users.noreply.github.com>
2023-04-22 23:39:56 -0400

    Update q_agent.py

--- src/q_agent.py
+++ src/q_agent.py
@@ -1,5 +1,7 @@
 import numpy as np
 from common.moves import Moves
+import os
+from grid_world import GridWorld
 
 
 class QAgent:
@@ -37,9 +39,51 @@ class QAgent:
                                           + self.alpha * (reward + self.gamma * max_q_value_in_new_state)
 
 
+
+    def show_q_table(self):
+        for x in range(self.environment.height):
+            for y in range(self.environment.width):
+                print("(", x, ",", y, ")")
+                print(self.q_table[x, y])
+
+
+    def download_q_table(self, file_name):
+        fiw = open(file_name, mode='w')
+        for x in range(self.environment.height):
+            for y in range(self.environment.width):
+                # fin.write(str(x) + ",")
+                # fin.write(str(y) + " ")
+                q_table_str = str(self.q_table[(x, y)])
+                q_table_str = " ".join(filter(str.isdigit, q_table_str))
+                fiw.write(q_table_str + "\n")
+        fiw.close()
+
+    def init_q_table(self, file_name):
+
+        fir = open(file_name, "r")
+        if os.path.getsize(file_name) != 0: #判断file是否为空，若不为空进行读入q table操作
+            for x in range(self.environment.height):
+                for y in range(self.environment.width):
+                    state = fir.readline()
+                    n = int(state[0])
+                    s = int(state[2])
+                    w = int(state[4])
+                    e = int(state[6])
+                    self.q_table[(x, y)] = {Moves.North.name: n, Moves.South.name: s, Moves.West.name: w,
+                                            Moves.East.name: e}
+            fir.close()
+        else:
+            print("Fail Initialization! The file is empty!")
+
 if __name__ == '__main__':
-    a = dict()
-    for x in range(4):
-        for y in range(4):
-            a[(x, y)] = {Moves.North.name: 0, Moves.South.name: 0, Moves.West.name: 0, Moves.East.name: 0}
-    print(a)
+    # a = dict()
+    # for x in range(4):
+    #     for y in range(4):
+    #         a[(x, y)] = {Moves.North.name: 0, Moves.South.name: 0, Moves.West.name: 0, Moves.East.name: 0}
+    # print(a)
+    file_name = 'q_table_states.txt'
+    g = GridWorld()
+    q = QAgent(g)
+    q.init_q_table(file_name)
+    # q.download_q_table(file_name)
+    q.show_q_table()
