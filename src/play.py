003bac0682ff8fe27501ce33f7bf6ac5a3e8db76
CharlotteSun0530 <charlottesun0530@gmail.com>
2023-04-22 20:17:23 -0400

    master-play

--- src/play.py
+++ src/play.py
@@ -1,53 +1,37 @@
 from grid_world import GridWorld
 from q_agent import Q_Agent
 
-def play(environment, agent, trials=500, max_steps_per_episode=1000, learn=False):
+
+def play(environment, agent, max_steps_per_episode=1000, learn=False):
     """The play function runs iterations and updates Q-values if desired."""
     reward_per_episode = []  # Initialise performance log
 
-    for trial in range(trials):  # Run trials
-        cumulative_reward = 0  # Initialise values of each game
-        step = 0
-        game_over = False
-        while step < max_steps_per_episode and game_over != True:  # Run until max steps or until game is finished
-            old_state = environment.current_location  # 获取当前状态
-            action = agent.choose_action(environment.actions)  # 选择行动
-            reward = environment.make_step(action)  # 执行行动并获取奖励
-            new_state = environment.current_location  # 获取新状态
+    cumulative_reward = 0  # Initialise values of each game
+    step = 0
+    game_over = False
+    while game_over != True:  # Run until max steps or until game is finished
+        old_state = environment.current_location
+        action = agent.choose_action(environment.actions)
+        reward = environment.make_step(action)
+        if reward == 0:
+            game_over = True
+        new_state = environment.current_location
 
-            if learn == True:  # 如果指定了学习，则更新 Q 值
-                agent.learn(old_state, reward, new_state, action)
+        if learn == True:  # Update Q-values if learning is specified
+            agent.learn(old_state, reward, new_state, action)
 
-            cumulative_reward += reward  # 累计奖励
-            step += 1
+        cumulative_reward += reward
+        step += 1
 
-            if environment.check_state() == 'TERMINAL':  # 如果游戏已结束，则结束当前游戏并开始下一个试验
-                environment.__init__()
-                game_over = True
+        if environment.check_state() == 'TERMINAL':  # If game is in terminal state, game over and start next trial
+            environment.__init__()
+            game_over = True
+
+        elif environment.check_state() == 'EXIT':  # 如果遇到了出口，则结束当前游戏并开始下一个试验
+            environment.__init__()
+            game_over = True
+
+    reward_per_episode.append(cumulative_reward)  # Append reward for current trial to performance log
 
 
-            elif environment.check_state() == 'EXIT':  # 如果遇到了出口，则结束当前游戏并开始下一个试验
-
-                # 修改部分：在到达出口后，计算所有位置的得分并选择最佳路径行走
-                scores = agent.get_scores(environment)
-                best_action = agent.get_best_action(environment, scores)
-                while best_action != None:
-                    action = best_action
-                    reward = environment.make_step(action)
-                    cumulative_reward += reward
-                    step += 1
-                    if environment.check_state() == 'TERMINAL':
-                        environment.__init__()
-                        game_over = True
-                        break
-                    elif environment.check_state() == 'EXIT':
-                        game_over = True
-                        break
-                    scores = agent.get_scores(environment)
-                    best_action = agent.get_best_action(environment, scores)
-                break
-
-        reward_per_episode.append(cumulative_reward)  # 将当前试验的累计奖励记录到性能日志中
-
-    return reward_per_episode  # 返回所有试验的累计奖励列表
-
+    return reward_per_episode  # Return performance log
